fc1b10fc17f489e22496469b72455e1834ed7685
a5hil <ashil555anto@gmail.com>
2026-07-09 20:17:14 +0530

    AddEmployee done

---
+++ src/Components/AddStudent.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import NavBar from './NavBar'
+
+const AddEmployee = () => {
+    return (
+        <div>
+            <NavBar />
+            <h2 className="text-center p-5">Add Employee</h2>
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <div className="row g-4">
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-label">Employee Code</label>
+                                <input type="text" className="form-control" />
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-label">Full Name</label>
+                                <input type="text" className="form-control" />
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-label">Email</label>
+                                <input type="email" className="form-control" />
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-label">Phone Number</label>
+                                <input type="tel" className="form-control" />
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-label">Designation</label>
+                                <input type="text" className="form-control" />
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-label">Date of Joining</label>
+                                <input type="date" className="form-control" />
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-label">Gender</label>
+                                <br />
+                                <label htmlFor="" className="form-check-label">Male</label>
+                                <input type="radio" className="form-check-input" name="Gender" id="male" />
+                                <label htmlFor="" className="form-check-label">Female</label>
+                                <input type="radio" className="form-check-input" name="Gender" id="female" />
+                            </div>                          
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-label">Address</label>
+                                <textarea className="form-control" rows="3"></textarea>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-label">Salary</label>
+                                <input type="number" className="form-control" />
+                            </div>
+                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                <button className="btn btn-dark">Submit</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default AddEmployee
